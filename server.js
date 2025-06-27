d037d73e238be620d62e1142390bcdb372e5a5a9
Tom <tomyanjasp@gmail.com>
2025-06-27 17:25:41 +0800

    route parameter error

--- server.js
+++ server.js
@@ -307,7 +307,7 @@ app.use((error, req, res, next) => {
 });
 
 // 404 handler
-app.use('*', (req, res) => {
+app.use((req, res) => {
   res.status(404).json({
     success: false,
     error: 'Route not found'
